91534b1bb557f7a1240bdb711ebed8b416069e6a
PAWAN SHARMA <105229626+pawansharma0702@users.noreply.github.com>
2022-12-22 20:12:37 +0530

    Add files via upload

---
+++ Sub.java
@@ -0,0 +1,16 @@
+public class Sum
+{
+public static void main(String [] args)
+{
+int a=10;
+int b=20;
+int d=45;
+int c=a+b;
+int d=a*b;
+int e=a-b;
+
+System.out.println("Sum of A and B"+c);
+System.out.println("Multiplication of A and B"+d);
+System.out.println("Diffrence  of A and B"+e);
+}
+}
---
+++ Sum.java
@@ -0,0 +1,17 @@
+public class Sum
+{
+public static void main(String [] args)
+{
+int a=10;
+int b=20;
+int d=30;
+String name="pawan";
+int c=a+b;
+int d=a*b;
+String name="PAWAN SHARMA";
+
+System.out.println("Sum of A and B"+c);
+System.out.println("Multiplication of A and B"+d);
+System.out.println("My name is ==> "+name)
+}
+}
